2e8d5d5f616834a37d668eb856c0152574715f01
YawObengAsante <yawobengasante@gmail.com>
2024-09-26 20:21:06 -0800

    initial commit

---
+++ middleware/errorHandler.js
@@ -0,0 +1,12 @@
+const { customAPIError } = require("../errors/custom-error");
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof customAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  return res
+    .status(500)
+    .json({ msg: "Something went wrong, please try again" });
+};
+
+module.exports = errorHandlerMiddleware;
